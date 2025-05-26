945bcab285451eb4314322ee9cbda1177a0d2f7d
Hiran <hirans2006@gmail.com>
2025-05-26 12:11:38 +0530

    make the add in notes api

--- backend/routes/notes/add.js
+++ backend/routes/notes/add.js
@@ -2,13 +2,13 @@ import db from '../../config/db.js';
 export default async (req, res) => {
   const { note } = req.body;
   console.log('Adding note:', note);
-  //   if (!note) {
-  //     res.status(401).json({ error: 'Note is required' });
-  //   }
-  //   try {
-  //     cons;
-  //   } catch (error) {
-  //     console.error('Error adding note:', error);
-  //     res.status(500).json({ error: 'Internal server error' });
-  //   }
+  if (!note) {
+    res.status(401).json({ error: 'Note is required' });
+  }
+  try {
+    cons;
+  } catch (error) {
+    console.error('Error adding note:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
