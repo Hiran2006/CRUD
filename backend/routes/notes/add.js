3e529a0267b5204bdcd647912e905305c58a2780
Hiran <hirans2006@gmail.com>
2025-05-26 13:08:28 +0530

    add note backend complete

--- backend/routes/notes/add.js
+++ backend/routes/notes/add.js
@@ -1,12 +1,16 @@
 import db from '../../config/db.js';
+import { getToken } from '../../config/jwt.js';
 export default async (req, res) => {
   const { note } = req.body;
-  console.log('Adding note:', note);
   if (!note) {
     res.status(401).json({ error: 'Note is required' });
   }
   try {
-    cons;
+    const id = getToken(req.cookies.token);
+    await db.query('INSERT INTO notes (user_id,note_text) VALUES ($1,$2)', [
+      id,
+      note,
+    ]);
   } catch (error) {
     console.error('Error adding note:', error);
     res.status(500).json({ error: 'Internal server error' });
